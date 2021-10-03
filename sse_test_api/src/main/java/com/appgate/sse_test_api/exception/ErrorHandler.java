58f31465558563dc3b06c93f21b59e76cb18103c
alexhunter28 <hector.martinez@avaldigitallabs.com>
2021-10-03 16:35:16 -0500

    Adding error handler

---
+++ sse_test_api/src/main/java/com/appgate/sse_test_api/exception/ErrorHandler.java
@@ -0,0 +1,20 @@
+package com.appgate.sse_test_api.exception;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler(value = {Exception.class})
+    public ResponseEntity<Object> handleExceptions(Exception ex, WebRequest request){
+        ErrorMessage errorMessage = new ErrorMessage(new Date(), ex.getMessage());
+        return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
